26926cd48ad53fdcf99828fe4291d0167ed9fb87
Xi LIU <xi.liu@u.nus.edu>
2026-04-11 21:36:29 +0800

    gen

--- jacobi_efnn/gen_script_gansu.py
+++ jacobi_efnn/gen_script_gansu.py
@@ -15,9 +15,17 @@ Path(outPath).mkdir(exist_ok=True,parents=True)
 
 num_layers_vec=[3,5,7,9,11]
 num_neurons_vec=[20,60,100,140,180]
-epochs=50000
-layer_neuron_pairs_vec=[[layer,neuron] for layer in num_layers_vec for neuron in num_neurons_vec]
-def contents_to_bash(num_layers,num_neurons,file_index):
+N_vec=[800,1600,3200]
+Q_vec=[2000,4000]
+eps_vec=[1e-3,1e-4]
+epochs=80000
+layer_neuron_N_Q_eps_vec=[[layer,neuron,N,Q,eps] for layer in num_layers_vec
+                      for neuron in num_neurons_vec
+                      for N in N_vec
+                      for Q in Q_vec
+                      for  eps in eps_vec
+                      ]
+def contents_to_bash(num_layers,num_neurons,N,Q,eps,file_index):
     print(f"num_layers={num_layers}, num_neurons={num_neurons}")
     contents = [
         "#!/bin/bash\n",
@@ -29,7 +37,7 @@ def contents_to_bash(num_layers,num_neurons,file_index):
         f"#SBATCH -o out_jacobi_efnn_layer{num_layers}_neuron{num_neurons}.out\n",
         f"#SBATCH -e out_jacobi_efnn_layer{num_layers}_neuron{num_neurons}.err\n",
         "cd /public/home/hkust_jwliu_1/liuxi/Documents/pyCode/efnn_based_networks/jacobi_efnn\n",
-        f"python3 -u train.py {epochs}  {num_layers} {num_neurons}\n",
+        f"python3 -u train.py {epochs}  {num_layers} {num_neurons} {N} {Q} {eps}\n",
     ]
     outBashName = outPath + f"/train_hs_efnn_layer{num_layers}_neuron{num_neurons}.sh"
     with open(outBashName, "w+") as fptr:
@@ -38,6 +46,6 @@ def contents_to_bash(num_layers,num_neurons,file_index):
 
 
 # Process each pair with its index
-for file_index, layer_neuron in enumerate(layer_neuron_pairs_vec):
-    num_layers, num_neurons=layer_neuron
-    contents_to_bash(num_layers, num_neurons, file_index)
+for file_index, val_item in enumerate(layer_neuron_N_Q_eps_vec):
+    num_layers,num_neurons,N,Q,eps=val_item
+    contents_to_bash(num_layers, num_neurons, N,Q,eps,file_index)
